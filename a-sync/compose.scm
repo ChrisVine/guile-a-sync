a0306dd099d55b1f2938e80488d0018e25edac09
Chris Vine <cvine@users.sourceforge.net>
2016-12-22 20:20:34 +0000

    Provide await-sleep! procedure

--- a-sync/compose.scm
+++ a-sync/compose.scm
@@ -62,14 +62,15 @@
 ;; further arguments as it requires.  All of the await-task!,
 ;; await-task-in-thread!, await-task-in-event-loop!, await-yield!,
 ;; await-generator!, await-generator-in-thread!,
-;; await-generator-in-event-loop!, await-timeout!, await-getline!,
-;; await-geteveryline!, await-getsomelines!, await-getblock!,
-;; await-geteveryblock!, await-getsomeblocks!, await-put-bytevector!
-;; and await-put-string!  procedures provided by the (a-sync
-;; event-loop) module are 'compose-a-sync'-capable.  In addition, to
-;; make an ordinary body of code which does not block (and which does
-;; not need to invoke a-sync's await procedure) usable by
-;; compose-a-sync, the no-await macro can be used to generate a
+;; await-generator-in-event-loop!, await-timeout!, await-sleep!,
+;; await-getline!, await-geteveryline!, await-getsomelines!,
+;; await-getblock!, await-geteveryblock!, await-getsomeblocks!,
+;; await-put-bytevector!, await-put-string!, await-accept! and
+;; await-connect! procedures provided by the (a-sync event-loop) and
+;; (a-sync sockets) modules are 'compose-a-sync'-capable.  In
+;; addition, to make an ordinary body of code which does not block
+;; (and which does not need to invoke a-sync's await procedure) usable
+;; by compose-a-sync, the no-await macro can be used to generate a
 ;; 'compose-a-sync'-capable procedure for it (see below).
 ;;
 ;; The await-glib-task, await-glib-task-in-thread,
