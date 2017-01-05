a2e0ce6d02eccc71ec1c66fad0899ab7c699aa86
Chris Vine <cvine@users.sourceforge.net>
2017-01-05 11:16:31 +0000

    Further documentation tweaks

--- a-sync/event-loop.scm
+++ a-sync/event-loop.scm
@@ -1249,10 +1249,6 @@
 ;; 'proc' runs it will do so as a separate event in the event loop and
 ;; so be multi-plexed with other events.
 ;;
-;; When 'proc' executes, 'await' and 'resume' will still be in use, so
-;; they may not be used by 'proc' to initiate other asynchronous
-;; operations.
-;;
 ;; This procedure must (like the a-sync procedure) be called in the
 ;; same thread as that in which the event loop runs.
 ;;
@@ -1266,6 +1262,10 @@
 ;; 'generator'.  This procedure can be useful for the purpose of
 ;; implementing co-operative multi-tasking, say by composing tasks
 ;; with compose-a-sync (see compose.scm).
+;;
+;; When 'proc' executes, 'await' and 'resume' will still be in use by
+;; this procedure, so they may not be reused by 'proc' (even though
+;; 'proc' runs in the event loop thread).
 ;;
 ;; This procedure calls event-post! in the event loop concerned.  This
 ;; is done in the same thread as that in which the event loop runs so
@@ -1590,12 +1590,12 @@
 ;; event loop passed in as an argument, or if none is passed (or #f is
 ;; passed), on the default event loop.
 ;;
-;; When 'proc' executes, 'await' and 'resume' will still be in use, so
-;; they may not be used by 'proc' to initiate other asynchronous
-;; operations.
-;;
 ;; This procedure must (like the a-sync procedure) be called in the
 ;; same thread as that in which the event loop runs.
+;;
+;; When 'proc' executes, 'await' and 'resume' will still be in use by
+;; this procedure, so they may not be reused by 'proc' (even though
+;; 'proc' runs in the event loop thread).
 ;;
 ;; Exceptions may propagate out of this procedure if they arise while
 ;; setting up (that is, before the first call to 'await' is made),
@@ -1775,12 +1775,12 @@
 ;; event loop passed in as an argument, or if none is passed (or #f is
 ;; passed), on the default event loop.
 ;;
-;; When 'proc' executes, 'await' and 'resume' will still be in use, so
-;; they may not be used by 'proc' to initiate other asynchronous
-;; operations.
-;;
 ;; This procedure must (like the a-sync procedure) be called in the
 ;; same thread as that in which the event loop runs.
+;;
+;; When 'proc' executes, 'await' and 'resume' will still be in use by
+;; this procedure, so they may not be reused by 'proc' (even though
+;; 'proc' runs in the event loop thread).
 ;;
 ;; Exceptions may propagate out of this procedure if they arise while
 ;; setting up (that is, before the first call to 'await' is made),
@@ -2066,12 +2066,12 @@
 ;; event loop passed in as an argument, or if none is passed (or #f is
 ;; passed), on the default event loop.
 ;;
-;; When 'proc' executes, 'await' and 'resume' will still be in use, so
-;; they may not be used by 'proc' to initiate other asynchronous
-;; operations.
-;;
 ;; This procedure must (like the a-sync procedure) be called in the
 ;; same thread as that in which the event loop runs.
+;;
+;; When 'proc' executes, 'await' and 'resume' will still be in use by
+;; this procedure, so they may not be reused by 'proc' (even though
+;; 'proc' runs in the event loop thread).
 ;;
 ;; Exceptions may propagate out of this procedure if they arise while
 ;; setting up (that is, before the first call to 'await' is made),
@@ -2208,12 +2208,12 @@
 ;; event loop passed in as an argument, or if none is passed (or #f is
 ;; passed), on the default event loop.
 ;;
-;; When 'proc' executes, 'await' and 'resume' will still be in use, so
-;; they may not be used by 'proc' to initiate other asynchronous
-;; operations.
-;;
 ;; This procedure must (like the a-sync procedure) be called in the
 ;; same thread as that in which the event loop runs.
+;;
+;; When 'proc' executes, 'await' and 'resume' will still be in use by
+;; this procedure, so they may not be reused by 'proc' (even though
+;; 'proc' runs in the event loop thread).
 ;;
 ;; Exceptions may propagate out of this procedure if they arise while
 ;; setting up (that is, before the first call to 'await' is made),
