ebf53c04311a00591c40b575a1adb43c7cc9a848
Chris Vine <cvine@users.sourceforge.net>
2017-04-14 15:58:16 +0100

    Add documentation for thread pool implementation

--- a-sync/compose.scm
+++ a-sync/compose.scm
@@ -65,13 +65,15 @@
 ;; await-generator-in-event-loop!, await-timeout!, await-sleep!,
 ;; await-getline!, await-geteveryline!, await-getsomelines!,
 ;; await-getblock!, await-geteveryblock!, await-getsomeblocks!,
-;; await-put-bytevector!, await-put-string!, await-accept! and
-;; await-connect! procedures provided by the (a-sync event-loop) and
-;; (a-sync sockets) modules are 'compose-a-sync'-capable.  In
-;; addition, to make an ordinary body of code which does not block
-;; (and which does not need to invoke a-sync's await procedure) usable
-;; by compose-a-sync, the no-await macro can be used to generate a
-;; 'compose-a-sync'-capable procedure for it (see below).
+;; await-put-bytevector!, await-put-string!, await-accept!,
+;; await-connect!, await-task-in-thread-pool! and
+;; await-generator-in-thread-pool! procedures provided by the (a-sync
+;; event-loop), (a-sync sockets) and (a-sync thread-pool) modules are
+;; 'compose-a-sync'-capable.  In addition, to make an ordinary body of
+;; code which does not block (and which does not need to invoke
+;; a-sync's await procedure) usable by compose-a-sync, the no-await
+;; macro can be used to generate a 'compose-a-sync'-capable procedure
+;; for it (see below).
 ;;
 ;; The meeting-send and meeting-receive procedures in the (a-sync
 ;; meeting) module also meet the 'compose-a-sync'-capable
--- a-sync/coroutines.scm
+++ a-sync/coroutines.scm
@@ -187,15 +187,19 @@
 ;; a-sync-read-watch!, await-getline!, await-geteveryline!,
 ;; await-getsomelines!, await-getblock!, await-geteveryblock!,
 ;; await-getsomeblocks!, a-sync-write-watch!, await-put-bytevector!,
-;; and await-put-string! convenience procedures which will correctly
+;; and await-put-string! convenience procedures, the sockets module
+;; has await-accept! and await-connect! procedures, and the
+;; thread-pool module has await-task-in-thread-pool! and
+;; await-generator-in-thread-pool! procedures, which will correctly
 ;; set this up for you automatically.  If those convenience procedures
 ;; are used, exceptions should always be handled locally in the
 ;; waitable procedure (and if the callback might throw, in the
 ;; callback also) if it is undesirable that uncaught exceptions
 ;; propagate out of event-loop-run!.  In the case of
-;; await-task-in-thread! and await-generator-in-thread!, those
-;; procedures also take an optional handler argument which will handle
-;; any exceptions thrown by the task or generator: otherwise a
+;; await-task-in-thread!, await-task-in-thread-pool!,
+;; await-generator-in-thread! and await-generator-in-thread-pool!,
+;; those procedures also take an optional handler argument which will
+;; handle any exceptions thrown by the task or generator: otherwise a
 ;; throwing thread would terminate the program if not caught.
 ;;
 ;; There can be as many calls to 'await' and asynchronous callbacks in
